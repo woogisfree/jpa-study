bf376327b667f8ce2dcffca3019a4779e32de1ee
woogie <woogisfree@gmail.com>
2024-03-13 21:22:19 +0900

    주문, 주문상품 엔티티 개발

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -35,7 +35,7 @@ public abstract class Item {
         this.stockQuantity += quantity;
     }
 
-    public void minusStock(int quantity) {
+    public void removeStock(int quantity) {
         int restStock = this.stockQuantity - quantity;
         if (restStock < 0) {
             throw new NotEnoughStockException("need more stock");
